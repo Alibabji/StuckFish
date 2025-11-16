1afe0f4918a49c98cbed343ef1d2f524f73b62ac
Alibabji <woodzin070929@gmail.com>
2025-11-16 14:27:57 +0900

    Fix env problem and wget problem

--- src/main.py
+++ src/main.py
@@ -3,39 +3,101 @@ from chess import Move
 import random
 import time, os, subprocess
 
-setup = subprocess.Popen(
-    ['sh', '-c', './scripts/setup.sh'],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.DEVNULL,
-    text=True,
-    bufsize=1,
-)
-for line in setup.stdout:
-    if not line:
-        break
-    print(line, end='', flush=True)
-retcode = setup.wait()
-if retcode not in [0, 2]:
-    raise RuntimeError(f"setup failed with exit code {retcode}")
+import pathlib
+import shutil
+import requests
+import zipfile
 
-LIBTORCH = os.path.join(os.getcwd(), "libtorch")
-os.environ["LIBTORCH"] = LIBTORCH
-# PATH
+def run_with_out(cmd, **kwargs):
+    p = subprocess.Popen(
+        cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        text=True,
+        bufsize=1,
+        **kwargs,
+    )
+    print('===Run subprocess', cmd, flush=True)
+    for line in p.stdout:
+        if not line:
+            break
+        print(line, end='', flush=True)
+    print('===Done subprocess', cmd, flush=True)
+    return p.wait()
+
+def run_with_err(cmd, **kwargs):
+    p = subprocess.Popen(
+        cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE,
+        text=True,
+        bufsize=1,
+        **kwargs,
+    )
+    print('===Run subprocess', cmd, flush=True)
+    for line in p.stderr:
+        if not line:
+            break
+        print(line, end='', flush=True)
+    print('===Done subprocess', cmd, flush=True)
+    return p.wait()
+
+def download(url, path):
+    r = requests.get(url)
+    print(path, " downloading...", flush = True)
+    with open(path, 'wb') as f:
+        f.write(r.content)
+    if os.path.isfile(path):
+        print(path, "downloaded.")
+    else:
+        print("FAILED DOWNLOAD", path)
+        exit(1)
+
+download('https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.9.0%2Bcpu.zip', 'libtorch.zip')
+with zipfile.ZipFile('libtorch.zip', 'r') as zf:
+    zf.extractall('.')
+
+env = os.environ.copy()
+LIBTORCH = pathlib.Path(os.getcwd()) / 'libtorch'
+print(LIBTORCH)
+env["LIBTORCH"] = LIBTORCH
 old_path = os.environ.get("PATH", "")
-os.environ["PATH"] = f"{LIBTORCH}/bin" + (f":{old_path}" if old_path else "")
-# LD_LIBRARY_PATH
+env["PATH"] = f"{LIBTORCH}/bin" + (f":{old_path}" if old_path else "")
 old_ld = os.environ.get("LD_LIBRARY_PATH", "")
-os.environ["LD_LIBRARY_PATH"] = f"{LIBTORCH}/lib" + (f":{old_ld}" if old_ld else "")
+env["LD_LIBRARY_PATH"] = f"{LIBTORCH}/lib" + (f":{old_ld}" if old_ld else "")
+
+print("Libtorch set.")
+
+download('https://sh.rustup.rs', 'rust.sh')
+run_with_err(
+    ['sh', 'rust.sh', '-qy'],
+    env=env
+)
+
+home = pathlib.Path(os.environ['HOME'])
+env["PATH"] = f'{home / ".cargo/bin"}' + f':{env["PATH"]}'
+
+run_with_err(
+    ['cargo', 'build', '--release', '--bin', 'suckfish'],
+    env=env,
+    cwd='engine',
+)
 
 engine = subprocess.Popen(
-    [os.path.join(os.getcwd(), 'suckfish'), '--nnue-path', os.path.join(os.getcwd(), 'nnue.ot')],
+    ['engine/target/release/suckfish', '--nnue-path', os.path.join(os.getcwd(), 'nnue.ot')],
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
     stderr=subprocess.DEVNULL,
     text=True,
     bufsize=1,
+    env=env,
 )
+# for line in engine.stderr:
+#     if not line:
+#         break
+#     print(line, end='', flush=True)
 print('============== Done setup ==============')
 
 @chess_manager.entrypoint
