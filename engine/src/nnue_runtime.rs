bc63c9b0f6701685666777185adc213bb3b3ebf9
Alibabji <woodzin070929@gmail.com>
2025-11-16 22:49:23 +0900

    Edit NNUE

--- engine/src/nnue_runtime.rs
+++ engine/src/nnue_runtime.rs
@@ -9,7 +9,6 @@ pub struct NnueRuntime {
     view: SparseAccumulatorRuntime,
     hidden0: LinearLayer,
     hidden1: LinearLayer,
-    hidden2: LinearLayer,
     output: LinearLayer,
 }
 
@@ -30,13 +29,11 @@ impl NnueRuntime {
         )?;
         let hidden0 = LinearLayer::from_linear(&layers.hidden0)?;
         let hidden1 = LinearLayer::from_linear(&layers.hidden1)?;
-        let hidden2 = LinearLayer::from_linear(&layers.hidden2)?;
         let output = LinearLayer::from_linear(&layers.output)?;
         Ok(Self {
             view,
             hidden0,
             hidden1,
-            hidden2,
             output,
         })
     }
@@ -51,8 +48,6 @@ impl NnueRuntime {
         x = self.hidden0.forward(&x);
         clipped_relu(&mut x);
         x = self.hidden1.forward(&x);
-        clipped_relu(&mut x);
-        x = self.hidden2.forward(&x);
         clipped_relu(&mut x);
         let out = self.output.forward(&x);
         let score = out
